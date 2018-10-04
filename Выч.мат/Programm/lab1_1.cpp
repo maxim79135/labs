ea52056e520a2c8be2e2af8b6367c5fbfab3b721
Maxim <maixm79135@gmail.com>
2018-10-04 15:26:49 +0300

    2

--- Выч.мат/Programm/lab1_1.cpp
+++ Выч.мат/Programm/lab1_1.cpp
@@ -8,8 +8,7 @@ double f_1(double x) { return 2 * x - 1 / (x + 1); }
 double f_2(double x) { return 2 + 1 / (x + 1) * (x + 1); }
 
 int main() {
-	double a, b, e;
-	cin >> a >> b >> e;
+	double a = 0.1, b = 0.9, e = 0.000001;
 	while (abs(a - b ) > 2 * e) {
 		if (f_1(a) * f_2(a) < 0) a = a - f(a) * (a - b) / (f(a) - f(b));
 		if (f_1(a) * f_2(a) > 0) a = a - f(a) / f_1(a);
--- Выч.мат/Programm/lab1_2.cpp
+++ Выч.мат/Programm/lab1_2.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-#include <gl/GL.h>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) { return x * x - log(x + 1); }
+
+double find(double x, double e) {
+	double rez;
+	int iteration;
+	cout << "x0 = " << x << endl;
+	do {
+		iteration++;
+		rez = x;
+		x = sqrt(log(x + 1));
+	} while (fabs(rez - x) > e && iteration < 10000000);
+	return x;
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	double a = 0.5, b = 0.9, e = 0.000001;	
+	cout << find(a, e);
 	return 0;
 }
