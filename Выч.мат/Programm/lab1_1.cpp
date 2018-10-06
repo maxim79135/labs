ee2bacdb84dcac9d6f43435ef94976e20169feb2
Maxim <maixm79135@gmail.com>
2018-10-06 12:16:13 +0300

    3

--- Выч.мат/Programm/lab1_1.cpp
+++ Выч.мат/Programm/lab1_1.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
 double f(double x) { return x * x - log(x + 1); }
 double f_1(double x) { return 2 * x - 1 / (x + 1); }
-double f_2(double x) { return 2 + 1 / (x + 1) * (x + 1); }
+double f_2(double x) { return 2 + 1 / ((x + 1) * (x + 1)); }
 
 int main() {
-	double a = 0.1, b = 0.9, e = 0.000001;
+	double a = 0.1, b = 0.9, e = 0.00001;
+	cout << "F(x) = x^2 - ln(x+1)" << endl;
+	cout << "Combined solution" << endl;
+	cout << "Isolation interval [" << a << ";" << b << "]" << endl;
+	cout << "Calculation error: " << e << endl;
+	cout << "F(a) = " << f(a) << endl;
+	cout << "F(b) = " << f(b) << endl; 
+	cout << "F'(a) = " << f_1(a) << endl;
+	cout << "F'(b) = " << f_1(b) << endl;
+	cout << "F''(a) = " << f_2(a) << endl;
+	cout << "F''(b) = " << f_2(b) << endl;	
+	int n = 1;
+	//if (!max(abs(f_2(a)), abs(f_2(b))) <= 2 * max(abs(f_1(a)), abs(f_1(b)))) b = b - (b - a) / 2;
+	
+	if (f(a) * f_2(a) > 0) {
+			a = a - f(a) / f_1(a);
+			b = b - f(b) * (b - a) / (f(b) - f(a));
+	} else {
+			b = b - f(b) / f_1(b);
+			a = a - f(a) * (a - b) / (f(a) - f(b));
+	}
+
 	while (abs(a - b ) > 2 * e) {
-		if (f_1(a) * f_2(a) < 0) a = a - f(a) * (a - b) / (f(a) - f(b));
-		if (f_1(a) * f_2(a) > 0) a = a - f(a) / f_1(a);
-		
-		if (f_1(b) * f_2(b) < 0) b = b - f(b) * (b - a) / (f(b) - f(a));
-		if (f_1(b) * f_2(b) > 0) b = b - f(b) / f_1(b);
+		a = a - f(a) / f_1(a);
+		b = b - f(b) * (b - a) / (f(b) - f(a));
+		cout << "n= " << fixed << setprecision(6) << n << "  a = " << setw(10) << a << " b = " << setw(10) << b << " |b-a|" << setw(10) << abs(b - a) << endl;
+		n++;
 	}
 
 	double x = (a + b) / 2;
-	cout << x;
+	cout << "Equation solution " << x;
 }
--- Выч.мат/Programm/lab1_2.cpp
+++ Выч.мат/Programm/lab1_2.cpp
@@ -3,23 +3,38 @@
 
 using namespace std;
 
+
 double f(double x) { return x * x - log(x + 1); }
+double f_1(double x) { return 2 * x - 1 / (x + 1); }
+double f_2(double x) { return 2 + 1 / (x + 1) * (x + 1); }
 
 double find(double x, double e) {
 	double rez;
 	int iteration;
-	cout << "x0 = " << x << endl;
 	do {
 		iteration++;
 		rez = x;
 		x = sqrt(log(x + 1));
-	} while (fabs(rez - x) > e && iteration < 10000000);
+	} while (fabs(rez - x) > e);
 	return x;
 }
 
 int main(int argc, char const *argv[])
 {
 	double a = 0.5, b = 0.9, e = 0.000001;	
-	cout << find(a, e);
+	cout << "F(x) = x^2 - ln(x+1)" << endl;
+	cout << "Combined solution" << endl;
+	cout << "Isolation interval [" << a << ";" << b << "]" << endl;
+	cout << "Calculation error: " << e << endl;
+	cout << "F(a) = " << f(a) << endl;
+	cout << "F(b) = " << f(b) << endl; 
+	cout << "F'(a) = " << f_1(a) << endl;
+	cout << "F'(b) = " << f_1(b) << endl;
+	cout << "F''(a) = " << f_2(a) << endl;
+	cout << "F''(b) = " << f_2(b) << endl;
+	cout << "phi(x) = sqrt(ln(x + 1)" << endl;
+	double x0 = (a + b) / 2;
+	cout << "phi'(x0) = " << 1 / (2 * sqrt(log(x0 + 1)) * (x0 + 1)) << endl;
+	cout << find(x0, e);
 	return 0;
 }
