f46f52cdabeae8ea83c7838522be30318b78f060
Maxim <maixm79135@gmail.com>
2018-11-17 20:41:59 +0300

    17.11.2018

--- Выч.мат/Programm/lab2_2.cpp
+++ Выч.мат/Programm/lab2_2.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-const	int n = 4;	
-const double e = 0.0001;
+const	int n = 3;	
+const double e = 0.01;
 
 bool converge(double *xk, double *xkp) {
 	double norm = 0;
@@ -15,12 +15,13 @@ bool converge(double *xk, double *xkp) {
 
 int main() {
 	cout << "Solving a fourth-order linear equation system by the Zeidel method" << endl;
+	
 	double A [4][4] = { 	{0.17, 0.31, -0.18, 0.22}, 
 	                      {-0.21, 0, 0.33, 0.22}, 
 									      {0.32, -0.18, 0.05, -0.19}, 
 									      {0.12, 0.28, -0.14, 0} 
 											 };
-
+	
 	double B[4] = {-1.71, 0.62, -0.89, 0.94};
 	double x[4], p[4];
 
@@ -38,11 +39,4 @@ int main() {
 	while (!converge(x, p));
 
 	for (int i = 0; i < n; i++) cout << "x" << i + 1 << " = " << x[i] << " | ";
-
-	/*
-	cout << endl;
-	for (int i = 0; i < n; i++) {
-		cout << A[i][0] * (-1.16506) + A[i][1] * (0.574248) + A[i][2] * (-1.67725) + A[i][3] * 1.1958 + B[i] << endl;
-	}
-	*/
 }
