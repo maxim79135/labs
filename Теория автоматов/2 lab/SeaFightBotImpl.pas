f434b0fdb10fa746a498a9e60d5daa2fbc01e2f3
Maxim <maixm79135@gmail.com>
2018-12-17 00:58:36 +0300

    17.12.2018

--- Теория автоматов/2 lab/SeaFightBotImpl.pas
+++ Теория автоматов/2 lab/SeaFightBotImpl.pas
@@ -9,7 +9,7 @@ type
   TByteArray2D = array of TByteArray;
   
 var main_array : array [0..9, 0..9] of byte;
-  
+  n : integer;  
 const
   EMPTY = 0;
   
@@ -31,12 +31,29 @@ implementation
 
 function getMap : TByteArray2D;
 begin
-
+  result := TByteArray2D.Create(
+      TByteArray.Create(0, 1, 0, 0, 0, 0, 1, 0, 0, 0),
+      TByteArray.Create(0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
+      TByteArray.Create(1, 1, 1, 0, 0, 0, 1, 0, 0, 1),
+      TByteArray.Create(0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+      TByteArray.Create(0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+      TByteArray.Create(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+      TByteArray.Create(1, 1, 0, 0, 0, 1, 0, 0, 1, 1),
+      TByteArray.Create(0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+      TByteArray.Create(0, 0, 0, 0, 0, 1, 0, 1, 0, 0),
+      TByteArray.Create(0, 0, 1, 0, 0, 1, 0, 0, 0, 0)
+  );
 end;
 
 function shoot : TByteArray;
+var i, j : integer;
 begin
-  TByteArray.Create(0);
+  for i := 0 to 9 do 
+    for j := 0 to 9 do
+      if (main_array[i, j] = 0) then begin
+        main_array[i, j] := 1;
+        result := TByteArray.Create(i, j);
+      end;
 end;
 
 procedure shootResult(resultCode : integer);
@@ -50,8 +67,11 @@ begin
 end;
 
 procedure SetParams(setsPerGame : integer);
+var i, j : integer;
 begin
-
+  for i := 0 to 9 do 
+    for j := 0 to 9 do
+      main_array[i, j] := 0;
 end;
 
 procedure onCurrentSetEnd;
